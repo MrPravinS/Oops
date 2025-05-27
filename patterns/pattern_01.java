3ed1092f0934db450ff637a89e753b871a8886a9
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-27 20:47:38 +0530

    create patterns in java

---
+++ patterns/pattern_01.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class pattern_01 {
+    public static void pattern(int n){
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+      }
+    }
+    public static void main(String[] args) {
+        
+        //  PatternPrint obj = new PatternPrint();
+        //  obj.pattern(5);
+        pattern(5);
+
+    }
+}
+
+
