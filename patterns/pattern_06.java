be27fe42741a35026bb2bc0900f9914903394e42
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-27 20:57:14 +0530

    pattern 6

---
+++ patterns/pattern_06.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class pattern_06 {
+     public static void pattern04(int n){
+      for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n-i+1; j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+      }
+    }
+
+    public static void main(String[] args) {
+        pattern04(5);
+    }
+}
