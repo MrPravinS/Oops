1266988848c852ab518aa0ff9f9b39bd94c51ddd
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-30 11:41:55 +0530

    create class Student

--- Basics/Basics.java
+++ Basics/Basics.java
@@ -1,4 +1,5 @@
 package Basics;
+/* 
 class Car {
     String color;
     int speed;
@@ -18,4 +19,36 @@ public class Basics {
         System.out.println("Color Of the car is : " + myCar.color);
         myCar.drive();
     }
+}
+*/
+
+class Student{
+    String name;
+    int age;
+    String grade;
+
+    void displayInfo(){
+        System.out.println("Student Name : " + name + " \nAge: "+ age + "\nGrade : " + grade);
+    }
+}
+
+public class Basics {
+ public static void main(String[] args) {
+     Student student1 = new Student();
+     Student student2 = new Student();
+     student1.name = "Pravin";
+     student1.age = 22;
+     student1.grade = "A+";
+
+     student2.name = "Ajay";
+     student2.age = 23;
+     student2.grade = "A";
+
+
+     student1.displayInfo();
+     student2.displayInfo();
+
+
+ }
+    
 }
