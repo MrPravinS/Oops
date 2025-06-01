0206aa52625f075fbd0a8a2adca0321ac99af232
MrPravinS <shegamwarpravin07@gmail.com>
2025-06-02 00:45:29 +0530

    Abstraction with  interface

---
+++ Abstract/part_04.java
@@ -0,0 +1,28 @@
+package Abstract;
+
+
+interface Playable{
+    void play();
+}
+
+ class Guitar implements Playable{
+   public void play(){
+        System.out.println("Guitar is playing....");
+    }
+ }
+
+
+ class Piano implements Playable{
+   public void play(){
+        System.out.println("Piano is playing....");
+    }
+ }
+public class part_04 {
+public static void main(String[] args) {
+    Playable g = new Guitar();
+    Playable p = new Piano();
+    g.play();
+    p.play();
+}
+    
+}
